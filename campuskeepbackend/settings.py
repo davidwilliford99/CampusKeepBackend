8d3150831c87fb62b2d856205140f958fc504b9c
davidwilliford99 <d9899w@gmail.com>
2023-11-27 18:45:45 -0500

    CORS Policy change

--- campuskeepbackend/settings.py
+++ campuskeepbackend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'campuskeepbackend'
+    'campuskeepbackend',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'campuskeepbackend.urls'
@@ -127,3 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# CORS Settings
+CORS_ORIGIN_ALLOW_ALL = True
